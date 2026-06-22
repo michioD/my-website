da8e0e83ad41d93ab69f1dfb4b1fa5d5cc763c68
michioD <l.m.frykman@gmail.com>
2026-06-22 17:31:10 -0600

    edit: remove project card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -60,7 +60,7 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Blog Section */}
+      {/* Blog Section
       <section id="blog" className="py-16 border-t border-slate-100">
         <div className="flex items-center justify-between mb-10">
           <h2 className="text-2xl font-bold text-slate-900">Writing & Notes</h2>
@@ -71,7 +71,7 @@ export default function Home() {
             <BlogCard key={post.id} post={post} />
           ))}
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
